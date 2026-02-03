d704f13f08465365509e1142a0074422a0ee649b
Pedro <pedrohfagundes2002@gmail.com>
2026-02-03 14:21:30 -0300

    remove image handling from media player

--- script.js
+++ script.js
@@ -9,20 +9,13 @@ function tocarMidia() {
   player.innerHTML = "";
   const item = midias[index];
 
-  if (item.type === "image") {
-    const img = document.createElement("img");
-    img.src = item.src;
-    player.appendChild(img);
-
-    setTimeout(proximaMidia, item.tempo);
-  } else {
     const video = document.createElement("video");
     video.src = item.src;
     video.autoplay = true;
     video.muted = true; // importante pra autoplay em TVs
     video.onended = proximaMidia;
     player.appendChild(video);
-  }
+
 }
 
 function proximaMidia() {

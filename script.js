1045783af4e7dc9e7f1b1ccd62e5abd85dbaf8a0
Pedro <pedrohfagundes2002@gmail.com>
2026-02-03 14:27:27 -0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const midias = [
-  { type: "video", src: "videos/segunraca_trabalho_ww1280_wh720_wr15.mp4" }
+    { type: "video", src: "videos/segunraca_trabalho_ww1280_wh720_wr15.mp4" },
+    { type: "video", src: "videos/cipa_resultado_ww1280_wh720_wr15.mp4" }
 ];
 
 let index = 0;
@@ -8,14 +9,21 @@ const player = document.getElementById("player");
 function tocarMidia() {
   player.innerHTML = "";
   const item = midias[index];
+    console.log(item);
+  if (item.type === "image") {
+    const img = document.createElement("img");
+    img.src = item.src;
+    player.appendChild(img);
 
+    setTimeout(proximaMidia, item.tempo);
+  } else {
     const video = document.createElement("video");
     video.src = item.src;
     video.autoplay = true;
     video.muted = true; // importante pra autoplay em TVs
     video.onended = proximaMidia;
     player.appendChild(video);
-
+  }
 }
 
 function proximaMidia() {
